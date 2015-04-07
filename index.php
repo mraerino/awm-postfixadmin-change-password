daa6380a7486f7271febde792bc355ac2f9fdb07
Marcus Weiner <marcus@mlsrv.de>
2015-04-08 01:18:30 +0200

    initial code commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ *
+ * Distributed under the terms of the license described in COPYING
+ *
+ */
+
+class_exists('CApi') or die();
+
+CApi::Inc('common.plugins.change-password');
+
+class CCustomChangePasswordPlugin extends AApiChangePasswordPlugin
+{
+	/**
+	 * @param CApiPluginManager $oPluginManager
+	 */
+	public function __construct(CApiPluginManager $oPluginManager)
+	{
+		parent::__construct('1.0', $oPluginManager);
+	}
+
+	/**
+	 * @param CAccount $oAccount
+	 * @return bool
+	 */
+	public function validateIfAccountCanChangePassword($oAccount)
+	{
+		$bResult = false;
+		if ($oAccount instanceof CAccount)
+		{
+			$bResult = true;
+		}
+
+		return $bResult;
+	}
+
+	/**
+	 * @param CAccount $oAccount
+	 * @return bool
+	 */
+	public function ChangePasswordProcess($oAccount)
+	{
+		$bResult = false;
+		if (0 < strlen($oAccount->PreviousMailPassword) &&
+			$oAccount->PreviousMailPassword !== $oAccount->IncomingMailPassword)
+		{
+
+			$aPostfixadmin = [
+				"host" => CApi::GetConf('plugins.postfixadmin-change-password.config.host', '127.0.0.1'),
+				"dbuser" => CApi::GetConf('plugins.postfixadmin-change-password.config.dbuser', 'root'),
+				"dbpassword" => CApi::GetConf('plugins.postfixadmin-change-password.config.dbpassword', ''),
+				"dbname" => CApi::GetConf('plugins.postfixadmin-change-password.config.dbname', 'postfixadmin'),
+			];
+
+			//connect to ispconfig database
+			$mysqlcon=mysqli_connect($aPostfixadmin['host'],$aPostfixadmin['dbuser'],$aPostfixadmin['dbpassword'],$aPostfixadmin['dbname']);
+
+		 	if($mysqlcon){
+				//check old pass is correct
+				$username = $oAccount->IncomingMailLogin;
+				$password = $oAccount->PreviousMailPassword;
+				$new_password = $oAccount->IncomingMailPassword;
+
+				$sql = "SELECT * FROM mailbox WHERE username='$username'";
+				$result = mysqli_query($mysqlcon,$sql);
+				$mailuser = mysqli_fetch_array($result);
+
+				$saved_password = $mailuser['password'];
+
+				// load md5crypt
+				include_once __DIR__.'/md5crypt.php';
+
+				//* Check if mailuser password is correct
+				if(md5crypt(stripslashes($password)) == $saved_password) {
+					$mailuser_id = $mailuser['username'];
+
+					$new_password = md5crypt($new_password);
+					$sql = "UPDATE mailbox SET password='$new_password' WHERE username='$mailuser_id'";
+					$result = mysqli_query($mysqlcon,$sql);
+
+					if (!$result){
+						//password update error
+						throw new CApiManagerException(Errs::UserManager_AccountNewPasswordUpdateError);
+					}
+				} else {
+					//old and new passwords dont match
+					throw new CApiManagerException(Errs::UserManager_AccountOldPasswordNotCorrect);
+				}
+				//disconnect from database
+				mysqli_close($mysqlcon);
+
+			} else {
+				//could not connect to database
+				throw new CApiManagerException(Errs::UserManager_AccountNewPasswordUpdateError);
+			}
+
+		}
+
+		return $bResult;
+	}
+}
+
+return new CCustomChangePasswordPlugin($this);
